a5634b26a46f290081d537f19486de6f6d9472e0
Cheerx <cheerx1994@163.com>
2016-10-31 15:41:50 +0800

    pager test

--- assets/main.js
+++ assets/main.js
@@ -5,11 +5,11 @@
 $(document).ready(function () {
     var pdfWrapper = $("#pdfWrapper");
     $("#rootContainer").css("min-height", $(window).height() - 232);
-    pdfWrapper.height($(window).height() - 229);
-
+    pdfWrapper.height($(window).height() - 172);
+    console.log($(window).height())
 })
 
 $(window).resize(function () {
     $("#rootContainer").css("min-height", $(window).height() - 232);
-    $("#pdfWrapper").height($(window).height() - 229)
+    $("#pdfWrapper").height($(window).height() - 172)
 })
